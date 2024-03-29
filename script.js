7cb6392820611ce2068393d4614e904e2b62bb6f
Hegel Motokoua <hyperpro200497@gmail.com>
2024-03-29 17:49:52 +0100

    nouvelle version

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ generateBtn.addEventListener("click", () => {
     let qrValue = qrInput.value.trim();
     if(!qrValue || preValue === qrValue) return;
     preValue = qrValue;
-    generateBtn.innerText = "Generating QR Code...";
+    generateBtn.innerText = "Génération de code QR...";
     qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
     qrImg.addEventListener("load", () => {
         wrapper.classList.add("active");
-        generateBtn.innerText = "Generate QR Code";
+        generateBtn.innerText = "Générer un code QR";
     });
 });
 
